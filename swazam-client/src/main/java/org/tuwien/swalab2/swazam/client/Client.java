14ca10ae5046345efa4d4b8fbaac169286f3a394
Michael Smolle <michael.smolle@gmail.com>
2013-01-10 17:15:28 +0100

    added some music peer has interactive terminal now

--- swazam-client/src/main/java/org/tuwien/swalab2/swazam/client/Client.java
+++ swazam-client/src/main/java/org/tuwien/swalab2/swazam/client/Client.java
@@ -1,6 +1,8 @@
 package org.tuwien.swalab2.swazam.client;
 
 import ac.at.tuwien.infosys.swa.audio.*;
+
+import java.awt.color.CMMException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -11,110 +13,112 @@ import javax.sound.sampled.AudioSystem;
 
 /**
  * Hello world!
- *
+ * 
  */
 public class Client {
 
-    public static void main(String[] args) {
-        Client client = new Client();
-        client.setUp();
-        client.startSwingUI();
+	public static void main(String[] args) {
+		Client client = new Client();
+		client.setUp();
+		client.startSwingUI();
 
-        client.startCLI(args);
+		client.startCLI(args);
 
+	}
 
-    }
+	private void setUp() {
+		System.out.println("Welcome to the SWAzam client.");
 
-    private void setUp() {
-        System.out.println("Welcome to the SWAzam client.");
+		// ToDo: somehow bootstrap
+	}
 
-        //ToDo: somehow bootstrap 
-    }
+	private void startSwingUI() {
+		// ToDo: start Swing UI
+	}
 
-    private void startSwingUI() {
-        //ToDo: start Swing UI
-    }
+	private void startCLI(String[] args) {
+		String cmd = "";
+		String filename = "";
+		Fingerprint fingerprint = null;
 
-    private void startCLI(String[] args) {
-        String cmd = "";
-        String filename = "";
-        Fingerprint fingerprint = null;
-        
-        usage();
+		usage();
 
-        boolean run = true;
-        while (run) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String input;
-            Scanner s = null;
-            
+		boolean run = true;
+		while (run) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			String input;
+			Scanner s = null;
 
-            try {
-                input = in.readLine();
-                s = new Scanner(input);
+			try {
+				input = in.readLine();
+				s = new Scanner(input);
 
-            } catch (IOException e) {
-                System.err.println("Couldn't read line.");
-            }
+			} catch (IOException e) {
+				System.err.println("Couldn't read line.");
+			}
 
-            if (s.hasNext()) {
-                cmd = s.next();
-            };
-            
-            if(cmd.equals("query")){
-                
-                filename = s.next();
-                
-                File file = new File(filename);
-                try{
-                if (file.getName().contains("mp3") || file.getName().contains("MP3")) {
-                     fingerprint = fingerprint(file);
-                }else{
-                    System.out.println("The file must be of type mp3 \n");
-                }
-                }catch(Exception e){
-                    System.out.println("Couldn't find file \n");
-                }
-            }else if(cmd.equals("usage")){
-                usage();
-            }else if (cmd.equals("quit")) {
-                run = false;
-            }else {
-                usage();
-            }
-        };
+			if (s.hasNext()) {
+				cmd = s.next();
+				
+				if (cmd.equals("query")) {
 
-        System.out.println("Shutting down Client");
+					filename = s.next();
 
-        }
+					File file = new File(filename);
+					try {
+						if (file.getName().contains("mp3")
+								|| file.getName().contains("MP3")) {
+							fingerprint = fingerprint(file);
+						} else {
+							System.out
+									.println("The file must be of type mp3 \n");
+						}
+					} catch (Exception e) {
+						System.out.println("Couldn't find file \n");
+					}
+				} else if (cmd.equals("usage")) {
+					usage();
+				} else if (cmd.equals("quit")) {
+					run = false;
+				} else {
+					usage();
+				}
+			}
+		}
 
+		System.out.println("Shutting down Client");
 
-    private void usage() {
-        System.out.println("\n Interactive commands:"
-                + "\n - query <path to mp3>"
-                + "\n - quit"
-                + "\n - usage"
-                + "\n");
-    }
+	}
 
-    private Fingerprint fingerprint(File file) {
-            
+	private void usage() {
+		System.out.println("\n Interactive commands:"
+				+ "\n - query <path to mp3>" 
+				+ "\n - quit" 
+				+ "\n - usage"
+				+ "\n");
+	}
 
-            try {
-                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+	private Fingerprint fingerprint(File file) {
 
-                System.out.println("Trying to fingerprint " + file.getName());
+		try {
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(file);
 
-                Fingerprint resultFingerpring = FingerprintSystem.fingerprint(audioInputStream);
+			System.out.println("Trying to fingerprint " + file.getName());
 
-                System.out.println("Fingerprinting of file " + file.getName() + " is: " + resultFingerpring.toString());
-                
-                return resultFingerpring;
-            } catch (Exception e) {
-                System.out.println("Somewhere something went horribly wrong...");
-                System.out.println(e.toString());
-            }
-            
-            return null;
-    }
+			Fingerprint resultFingerpring = FingerprintSystem
+					.fingerprint(audioInputStream);
+
+			System.out.println("Fingerprinting of file " + file.getName()
+					+ " is: " + resultFingerpring.toString());
+
+			return resultFingerpring;
+		} catch (Exception e) {
+			System.out.println("Somewhere something went horribly wrong...");
+			System.out.println(e.toString());
+		}
+
+		return null;
+	}
 }
---
+++ swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/Peer.java
@@ -0,0 +1,129 @@
+package org.tuwien.swalab2.swazam.peer;
+
+import ac.at.tuwien.infosys.swa.audio.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+
+/**
+ * Hello world!
+ * 
+ */
+public class Peer {
+
+	public static void main(String[] args) {
+		Peer peer = new Peer();
+		peer.setUp();
+		peer.startSwingUI();
+
+		peer.startCLI(args);
+
+	}
+
+	private void setUp() {
+		System.out.println("Welcome to the SWAzam client.");
+
+		// ToDo: somehow bootstrap
+	}
+
+	private void startSwingUI() {
+		// ToDo: start Swing UI
+	}
+
+	private void startCLI(String[] args) {
+		String cmd = "";
+		String filename = "";
+		Fingerprint fingerprint = null;
+
+		usage();
+
+		boolean run = true;
+		while (run) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			String input;
+			Scanner s = null;
+
+			try {
+				input = in.readLine();
+				s = new Scanner(input);
+
+			} catch (IOException e) {
+				System.err.println("Couldn't read line.");
+			}
+
+			if (s.hasNext()) {
+				cmd = s.next();
+
+				if (cmd.equals("add")) {
+					
+					if (s.hasNextInt()) {
+						// TODO: send file to peer
+						System.out.println("TODO: send file to peer");
+						Integer id = Integer.valueOf(s.next());
+					} else {
+						filename = s.next();
+						
+						File file = new File(filename);
+						try {
+							if (file.getName().contains("mp3")
+									|| file.getName().contains("MP3")) {
+								fingerprint = fingerprint(file);
+							} else {
+								System.out
+										.println("The file must be of type mp3 \n");
+							}
+						} catch (Exception e) {
+							System.out.println("Couldn't find file \n");
+						}
+					}
+
+				} else if (cmd.equals("usage")) {
+					usage();
+				} else if (cmd.equals("quit")) {
+					run = false;
+				} else {
+					usage();
+				}
+			}
+		}
+		System.out.println("Shutting down Client");
+
+	}
+
+	private void usage() {
+		System.out.println("\n Interactive commands:"
+				+ "\n - add <path to mp3>" 
+				+ "\n - add <id> <path to mp3>" 
+				+ "\n - quit" 
+				+ "\n - usage"
+				+ "\n");
+	}
+
+	private Fingerprint fingerprint(File file) {
+
+		try {
+			AudioInputStream audioInputStream = AudioSystem
+					.getAudioInputStream(file);
+
+			System.out.println("Trying to fingerprint " + file.getName());
+
+			Fingerprint resultFingerpring = FingerprintSystem
+					.fingerprint(audioInputStream);
+
+			System.out.println("Fingerprinting of file " + file.getName()
+					+ " is: " + resultFingerpring.toString());
+
+			return resultFingerpring;
+		} catch (Exception e) {
+			System.out.println("Somewhere something went horribly wrong...");
+			System.out.println(e.toString());
+		}
+
+		return null;
+	}
+}
