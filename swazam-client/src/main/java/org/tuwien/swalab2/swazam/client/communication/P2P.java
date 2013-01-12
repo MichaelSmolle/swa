fc7ff02106b5d5b0cdfeb81ebd98adfefb3a82ac
Michael Smolle <michael.smolle@gmail.com>
2013-01-12 19:09:18 +0100

    modified:   ../swazam-client/src/main/java/org/tuwien/swalab2/swazam/client/communication/P2P.java 	deleted:    lib/com/jtella/jtella/0.9/jtella-0.9.jar.lastUpdated 	deleted:    lib/com/jtella/jtella/0.9/jtella-0.9.pom.lastUpdated 	modified:   pom.xml 	modified:   src/main/java/org/tuwien/swalab2/swazam/peer/p2p/P2P.java

--- swazam-client/src/main/java/org/tuwien/swalab2/swazam/client/communication/P2P.java
+++ swazam-client/src/main/java/org/tuwien/swalab2/swazam/client/communication/P2P.java
@@ -1,6 +1,6 @@
 package org.tuwien.swalab2.swazam.client.communication;
 
-import com.kenmccrary.jtella.*;
+
 
 
 public class P2P {
--- swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/p2p/P2P.java
+++ swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/p2p/P2P.java
@@ -1,5 +1,7 @@
 package org.tuwien.swalab2.swazam.peer.p2p;
 
+import com.kenmccrary.jtella.*;
+
 public class P2P {
 
 }
