3dbd5e291f16d554fb178fbf0cafceca6a17a97d
archaon <johannesspiessberger@yahoo.de>
2013-01-19 16:54:54 +0100

    more p2p stuff

--- swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/MessageHandler.java
+++ swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/MessageHandler.java
@@ -2,6 +2,8 @@ package org.tuwien.swalab2.swazam.peer;
 
 public class MessageHandler {
 	
+	private ConnectionHandler ch;
+	
 	public MessageHandler() {
 		
 	}
@@ -9,6 +11,10 @@ public class MessageHandler {
 	public synchronized void handleMessage(Message m) {
 		if(m instanceof SearchMessage) {
 			System.out.println("stub");
+		} else if (m instanceof requestPeerReplyMessage) {
+			ch.addNodes(((requestPeerReplyMessage)m).getHostCache());
+		} else if (m instanceof requestPeerMessage) {
+			ch.replyToRequestNodes(m.getSender(), m.getSenderPort());
 		}
 	}
 }
