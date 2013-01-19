e83c74f94b391b4be29e05ebcbc475dcb0c609ee
Michael Smolle <michael.smolle@gmail.com>
2013-01-19 22:10:07 +0100

    modified:   src/main/java/org/tuwien/swalab2/swazam/peer/SearchReplyMessage.java

--- swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/SearchReplyMessage.java
+++ swazam-peer/src/main/java/org/tuwien/swalab2/swazam/peer/SearchReplyMessage.java
@@ -1,6 +1,5 @@
 package org.tuwien.swalab2.swazam.peer;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 public class SearchReplyMessage extends Message {
@@ -9,15 +8,26 @@ public class SearchReplyMessage extends Message {
 	 * 
 	 */
 	private static final long serialVersionUID = 8583493611380121826L;
-	private String artist;
-	private String title;
-	private String album;
+	private String filename = "";
+	private String artist = "";
+	private String title = "";
+	private String album = "";
 	
-	public SearchReplyMessage(String ip, int port, String id, String artist, String title, String album) throws UnknownHostException {
+	public SearchReplyMessage(String ip, int port, String id, String filename) throws UnknownHostException {
 		super(ip,port, id);
+		this.filename = filename;
+	}
+	
+	public SearchReplyMessage(String ip, int port, String id, String filename , String artist, String title, String album) throws UnknownHostException {
+		super(ip,port, id);
+		this.filename = filename;
 		this.artist = artist;
 		this.title = title;
 		this.album = album;
+	}
+	
+	public String getFilename() {
+		return this.filename;
 	}
 	
 	public String getArtist() {
