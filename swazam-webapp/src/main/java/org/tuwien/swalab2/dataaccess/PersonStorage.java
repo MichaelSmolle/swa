26f7005440ed5555444d2d891d089cc175e4dd98
Gregor Hagleitner <gregor.hagleitner@gmail.com>
2013-01-20 20:17:57 +0100

    testdata user1/pass1  user2/pass2

--- swazam-webapp/src/main/java/org/tuwien/swalab2/dataaccess/PersonStorage.java
+++ swazam-webapp/src/main/java/org/tuwien/swalab2/dataaccess/PersonStorage.java
@@ -14,6 +14,7 @@ import javax.faces.bean.ManagedBean;
 import javax.inject.Singleton;
 import org.tuwien.swalab2.swazam.util.model.entities.CryptoUtils;
 import org.tuwien.swalab2.swazam.util.model.entities.Person;
+import org.tuwien.swalab2.swazam.util.model.entities.SwazamAccount;
 
 /**
  *
@@ -34,6 +35,7 @@ public final class PersonStorage implements Serializable{
         if (personStorage == null) 
         {
             personStorage = new PersonStorage();
+            setUpTestData();
         }
         return personStorage;
     }
@@ -44,6 +46,32 @@ public final class PersonStorage implements Serializable{
         p.setPassWord(CryptoUtils.byteArrayToHexString(CryptoUtils.computeHash(p.getPassWord())));
      }catch(Exception e) {}
        persons.put(String.valueOf(p.getId()), p);
+    }
+    
+    private static synchronized void setUpTestData(){
+        Person p1 = new Person();
+        p1.setUserName("user1");
+        p1.setLastName("lastUsername1");
+        p1.setFirstName("firstname1");
+        p1.setPassWord("pass1");
+        
+        SwazamAccount newAccount = new SwazamAccount();               
+        newAccount.setBalance(10);
+        p1.setAccount(newAccount);
+        
+        
+        Person p2 = new Person();
+        p2.setUserName("user2");
+        p2.setLastName("lastUsername2");
+        p2.setFirstName("firstname2");
+        p2.setPassWord("pass2");
+        
+        SwazamAccount newAccount1 = new SwazamAccount();               
+        newAccount.setBalance(10);
+        p2.setAccount(newAccount1);
+        
+        getInstance().addPerson(p2);
+        getInstance().addPerson(p1);
     }
     
     public Person getPerson(String key){
